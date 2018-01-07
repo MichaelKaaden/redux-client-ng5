8ddc560f3b9e3657b8f3faefa2823c4d334b6dcd
Michael Kaaden <github@kaaden.net>
2018-01-07 10:54:49 +0100

    Add a material-module.ts containing some components as example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { CounterContainerComponent } from "./counter-container/counter-container.component";
+import { MaterialModule } from "./material-module";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
