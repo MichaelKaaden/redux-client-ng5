9253c8996e972beacc2ac6ef4b9058136a115693
Michael Kaaden <github@kaaden.net>
2018-01-07 10:55:19 +0100

    Add the container component

---
+++ src/app/counter-container/counter-container.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from "@angular/core";
+
+@Component({
+  selector: "mk-counter-container",
+  templateUrl: "./counter-container.component.html",
+  styleUrls: ["./counter-container.component.css"]
+})
+export class CounterContainerComponent implements OnInit {
+  @Input() counterIndex;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
