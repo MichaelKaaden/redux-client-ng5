d8eb4f8e5189ea6415528db85db2e2cd73f06243
Michael Kaaden <github@kaaden.net>
2018-01-09 22:42:57 +0100

    Call thunk middleware

--- src/app/counter-container/counter-container.component.ts
+++ src/app/counter-container/counter-container.component.ts
@@ -1,16 +1,37 @@
-import {Component, Input, OnInit} from "@angular/core";
+import { NgRedux } from "@angular-redux/store";
+import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
+import { CounterActionCreatorService } from "../actions/counter.action-creator.service";
+import { IAppState } from "../models/app-state";
 
 @Component({
   selector: "mk-counter-container",
   templateUrl: "./counter-container.component.html",
-  styleUrls: ["./counter-container.component.css"]
+  styleUrls: ["./counter-container.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterContainerComponent implements OnInit {
   @Input() counterIndex;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private redux: NgRedux<IAppState>,
+              private counterActionCreatorService: CounterActionCreatorService) {
   }
 
+  ngOnInit() {
+    this.load();
+  }
+
+  // needed to capture "this" properly
+  public decrement = (by: number): void => {
+    this.redux.dispatch<any>(this.counterActionCreatorService.decrement(this.counterIndex, by));
+  };
+
+  // needed to capture "this" properly
+  public increment = (by: number): void => {
+    this.redux.dispatch<any>(this.counterActionCreatorService.increment(this.counterIndex, by));
+  };
+
+  // needed to capture "this" properly
+  public load = (): void => {
+    this.redux.dispatch<any>(this.counterActionCreatorService.load(this.counterIndex));
+  };
 }
