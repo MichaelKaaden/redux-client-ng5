c6ad69d18f364eb6bb3a3a4a39ff30a1897328ea
Michael Kaaden <github@kaaden.net>
2018-01-09 23:11:28 +0100

    Get the counter from the Redux store

--- src/app/counter-container/counter-container.component.ts
+++ src/app/counter-container/counter-container.component.ts
@@ -1,7 +1,9 @@
 import { NgRedux } from "@angular-redux/store";
 import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Observable";
 import { CounterActionCreatorService } from "../actions/counter.action-creator.service";
 import { IAppState } from "../models/app-state";
+import { ICounter } from "../models/counter";
 
 @Component({
   selector: "mk-counter-container",
@@ -12,12 +14,17 @@ import { IAppState } from "../models/app-state";
 export class CounterContainerComponent implements OnInit {
   @Input() counterIndex;
 
+  public counter$: Observable<ICounter>;
+
   constructor(private redux: NgRedux<IAppState>,
               private counterActionCreatorService: CounterActionCreatorService) {
   }
 
   ngOnInit() {
     this.load();
+    this.counter$ = this.redux.select((state: IAppState) =>
+      state.counters.all.find((item: ICounter) =>
+        item.index === this.counterIndex));
   }
 
   // needed to capture "this" properly
