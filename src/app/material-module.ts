3ab548dec7dd6c60b1eeb19fb4cd758a9dbcfc73
Michael Kaaden <github@kaaden.net>
2018-01-17 20:56:12 +0100

    Add navigation

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 
-import { MatButtonModule, MatIconModule, MatProgressSpinnerModule } from "@angular/material";
+import { MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatTabsModule } from "@angular/material";
 
 @NgModule({
   imports: [
@@ -11,6 +11,7 @@ import { MatButtonModule, MatIconModule, MatProgressSpinnerModule } from "@angul
     MatButtonModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    MatTabsModule,
   ]
 })
 export class MaterialModule {
