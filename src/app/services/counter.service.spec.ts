67204e466dee6841afe38bd020c6f4397b2a604a
Michael Kaaden <github@kaaden.net>
2018-01-20 17:13:38 +0100

    Fix the CounterService test

--- src/app/services/counter.service.spec.ts
+++ src/app/services/counter.service.spec.ts
@@ -1,15 +1,21 @@
-import { TestBed, inject } from "@angular/core/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { inject, TestBed } from "@angular/core/testing";
 
 import { CounterService } from "./counter.service";
 
 describe("CounterService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CounterService]
+      providers: [CounterService],
+      imports: [HttpClientTestingModule]
     });
   });
 
-  it("should be created", inject([CounterService], (service: CounterService) => {
+  it("should be created", inject([
+    HttpTestingController,
+    CounterService
+  ], (http: HttpTestingController,
+      service: CounterService) => {
     expect(service).toBeTruthy();
   }));
 });
