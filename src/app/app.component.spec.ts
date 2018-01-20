ab0b91ee63abbc9d144d0497dfdc1d225e92d50c
Michael Kaaden <github@kaaden.net>
2018-01-20 17:13:56 +0100

    Fix the AppComponent test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { async, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
@@ -7,18 +9,23 @@ describe("AppComponent", () => {
       declarations: [
         AppComponent
       ],
+      imports: [RouterTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
+
   it("should create the app", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'Redux Demo Application'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual("Redux Demo Application");
   }));
+
   it("should render title in a h1 tag", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
