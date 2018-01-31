8327d0fa864dc2891bfd0112e8293de26573ce5c
Michael Kaaden <github@kaaden.net>
2018-01-31 21:45:30 +0100

    Rewrite existing Counter Action Creator Service test

--- src/app/actions/counter.action-creator.service.spec.ts
+++ src/app/actions/counter.action-creator.service.spec.ts
@@ -1,15 +1,39 @@
-import { inject, TestBed } from "@angular/core/testing";
+import { getTestBed, TestBed } from "@angular/core/testing";
+import { Observable } from "rxjs/Observable";
+import { Counter, ICounter } from "../models/counter";
+import { CounterService } from "../services/counter.service";
 
 import { CounterActionCreatorService } from "./counter.action-creator.service";
 
-xdescribe("CounterActionCreatorService", () => {
+const BASE_VALUE = 60;
+
+const counterServiceStub = {
+  counter: (index: number): Observable<ICounter> => {
+    console.log(`returning counter for index ${index}`);
+    return Observable.of(new Counter(index, index + BASE_VALUE));
+  }
+};
+
+describe("CounterActionCreatorService", () => {
+  let injector: TestBed;
+  let service: CounterActionCreatorService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CounterActionCreatorService]
+      providers: [
+        CounterActionCreatorService,
+        {
+          provide: CounterService,
+          useValue: counterServiceStub,
+        }
+      ]
     });
+
+    injector = getTestBed();
+    service = injector.get(CounterActionCreatorService);
   });
 
-  it("should be created", inject([CounterActionCreatorService], (service: CounterActionCreatorService) => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
