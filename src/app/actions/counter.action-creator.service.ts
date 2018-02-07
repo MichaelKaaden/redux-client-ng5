e8be5e61bcd196232463d2fc2c1170bb5229ccc5
Michael Kaaden <github@kaaden.net>
2018-02-07 22:15:31 +0100

    Dispatch error messages instead of just logging errors

--- src/app/actions/counter.action-creator.service.ts
+++ src/app/actions/counter.action-creator.service.ts
@@ -7,6 +7,7 @@ import { Counter, ICounter } from "../models/counter";
 import { CounterService } from "../services/counter.service";
 import {
   IDecrementedCounterAction,
+  IErrorAction,
   IIncrementedCounterAction,
   ILoadedAction,
   ILoadedAllAction,
@@ -32,25 +33,24 @@ export class CounterActionCreatorService {
    * @param {number} by The decrement value
    * @returns {Thunk}
    */
-  public decrement = (index: number, by = 1): Thunk => (dispatch: Dispatch<IAppState>, getState: () => IAppState) => {
-    if (index < 0) {
-      // TODO handle errors correctly, see Dan's answer in
-      // https://stackoverflow.com/questions/34403269/what-is-the-best-way-to-deal-with-a-fetch-error-in-react-redux
-      this.logError("decrement", `index "${index}" < 0`);
-      return;
-    }
+  public decrement = (index: number, by = 1): Thunk =>
+    (dispatch: Dispatch<IAppState>, getState: () => IAppState) => {
+      if (index < 0) {
+        return dispatch(this.buildErrorAction("decrement", `index ${index} < 0`));
+      }
 
-    // set "saving" for this counter
-    dispatch(this.buildSavingAction(index));
+      // set "saving" for this counter
+      dispatch(this.buildSavingAction(index));
 
-    this.counterService.decrementCounter(index, by)
-      .subscribe((c: ICounter) => {
-        const counter = new Counter(c.index, c.value);
-        dispatch(this.buildDecrementedAction(index, counter));
-      }, (error: HttpErrorResponse) => this.logError(
-        "decrement",
-        `decrementing the counter failed with ${error.error instanceof Error ? error.error.message : error.error}`));
-  };
+      this.counterService.decrementCounter(index, by)
+        .subscribe((c: ICounter) => {
+            const counter = new Counter(c.index, c.value);
+            return dispatch(this.buildDecrementedAction(index, counter));
+          }, (error: HttpErrorResponse) =>
+            dispatch(this.buildErrorAction("decrement",
+              `decrementing the counter failed with ${error instanceof Error ? error.message : error}`))
+        );
+    };
 
   /**
    * Thunk: Increment a counter by saving it to the RESTful Web Service.
@@ -59,23 +59,24 @@ export class CounterActionCreatorService {
    * @param {number} by The increment value
    * @returns {Thunk}
    */
-  public increment = (index: number, by = 1): Thunk => (dispatch: Dispatch<IAppState>, getState: () => IAppState) => {
-    if (index < 0) {
-      this.logError("increment", `index "${index}" < 0`);
-      return;
-    }
+  public increment = (index: number, by = 1): Thunk =>
+    (dispatch: Dispatch<IAppState>, getState: () => IAppState) => {
+      if (index < 0) {
+        return dispatch(this.buildErrorAction("increment", `index ${index} < 0`));
+      }
 
-    // set "saving" for this counter
-    dispatch(this.buildSavingAction(index));
+      // set "saving" for this counter
+      dispatch(this.buildSavingAction(index));
 
-    this.counterService.incrementCounter(index, by)
-      .subscribe((c: ICounter) => {
-        const counter = new Counter(c.index, c.value);
-        dispatch(this.buildIncrementedAction(index, counter));
-      }, (error: HttpErrorResponse) => this.logError(
-        "increment",
-        `incrementing the counter failed with ${error.error instanceof Error ? error.error.message : error.error}`));
-  };
+      this.counterService.incrementCounter(index, by)
+        .subscribe((c: ICounter) => {
+            const counter = new Counter(c.index, c.value);
+            return dispatch(this.buildIncrementedAction(index, counter));
+          }, (error: HttpErrorResponse) =>
+            dispatch(this.buildErrorAction("increment",
+              `incrementing the counter failed with ${error instanceof Error ? error.message : error}`))
+        );
+    };
 
   /***
    * Thunk: Load a counter from the RESTful Web Service.
@@ -83,50 +84,53 @@ export class CounterActionCreatorService {
    * @param {number} index The counter's index
    * @returns {Thunk}
    */
-  public load = (index: number): Thunk => (dispatch: Dispatch<IAppState>, getState: () => IAppState) => {
-    if (index < 0) {
-      this.logError("load", `index "${index}" < 0`);
-      return;
-    }
+  public load = (index: number): Thunk =>
+    (dispatch: Dispatch<IAppState>, getState: () => IAppState) => {
+      if (index < 0) {
+        return dispatch(this.buildErrorAction("load", `index ${index} < 0`));
+      }
 
-    // don't load the counter if it's already loaded
-    const cachedCounter: ICounter = getState().counters.all.find((item: ICounter) => item.index === index);
-    if (cachedCounter) {
-      return;
-    }
+      // don't load the counter if it's already loaded
+      const cachedCounter: ICounter = getState().counters.all.find((item: ICounter) => item.index === index);
+      if (cachedCounter) {
+        return;
+      }
 
-    // set "loading" for this counter
-    dispatch(this.buildLoadingAction(index));
+      // set "loading" for this counter
+      dispatch(this.buildLoadingAction(index));
 
-    this.counterService.counter(index)
-      .subscribe((c: ICounter) => {
-        const counter = new Counter(c.index, c.value);
-        dispatch(this.buildLoadedAction(index, counter));
-      }, (error: HttpErrorResponse) => this.logError(
-        "load",
-        `retrieving the counter failed with ${error.error instanceof Error ? error.error.message : error.error}`));
-  };
+      this.counterService.counter(index)
+        .subscribe((c: ICounter) => {
+            const counter = new Counter(c.index, c.value);
+            return dispatch(this.buildLoadedAction(index, counter));
+          }, (error: HttpErrorResponse) =>
+            dispatch(this.buildErrorAction("load",
+              `retrieving the counter failed with ${error instanceof Error ? error.message : error}`))
+        );
+    };
 
   /**
    * Thunk: Load all counters from the RESTful Web Service.
    *
    * @returns {Thunk}
    */
-  public loadAll = (): Thunk => (dispatch: Dispatch<IAppState>, getState: () => IAppState) => {
-    // set "loading" for this counter
-    dispatch(this.buildLoadingAllAction());
+  public loadAll = (): Thunk =>
+    (dispatch: Dispatch<IAppState>, getState: () => IAppState) => {
+      // set "loading" for this counter
+      dispatch(this.buildLoadingAllAction());
 
-    this.counterService.counters()
-      .subscribe((cs: ICounter[]) => {
-        const counters = [];
-        for (const c of cs) {
-          counters.push(new Counter(c.index, c.value));
-        }
-        dispatch(this.buildLoadedAllAction(counters));
-      }, (error: HttpErrorResponse) => this.logError(
-        "loadAll",
-        `retrieving all counters failed with ${error.error instanceof Error ? error.error.message : error.error}`));
-  };
+      this.counterService.counters()
+        .subscribe((cs: ICounter[]) => {
+            const counters = [];
+            for (const c of cs) {
+              counters.push(new Counter(c.index, c.value));
+            }
+            return dispatch(this.buildLoadedAllAction(counters));
+          }, (error: HttpErrorResponse) =>
+            dispatch(this.buildErrorAction("loadAll",
+              `retrieving all counters failed with ${error instanceof Error ? error.message : error}`))
+        );
+    };
 
   /*
    * Helper functions
@@ -195,7 +199,10 @@ export class CounterActionCreatorService {
     };
   };
 
-  private logError(methodName: string, message: string) {
-    console.error(`error in the "${methodName}" action creator: ${message}`);
-  }
+  private buildErrorAction: ActionCreator<IErrorAction> = (methodName: string, message: string) => {
+    return {
+      type: TypeKeys.ERROR,
+      error: `error in the "${methodName}" action creator: ${message}`,
+    };
+  };
 }
