046d12b1107d6b58fb7428c4df389f39b128f309
Michael Kaaden <github@kaaden.net>
2018-02-08 23:15:53 +0100

    Get the types right for both reducers

--- src/app/reducers/counter.reducer.ts
+++ src/app/reducers/counter.reducer.ts
@@ -1,126 +1,128 @@
+import { Reducer } from "redux";
 import { ActionTypes, TypeKeys } from "../actions/counter.actions";
 import { ICounterState, INITIAL_COUNTERS_STATE } from "../models/app-state";
 import { Counter, ICounter } from "../models/counter";
 
-export function counterReducer(state: ICounterState = INITIAL_COUNTERS_STATE, action: ActionTypes): ICounterState {
-  let counter: ICounter;
-  let newCounter: ICounter;
-  let newCounters: ICounter[] = [];
+export const counterReducer: Reducer<ICounterState> =
+  (state: ICounterState = INITIAL_COUNTERS_STATE, action: ActionTypes): ICounterState => {
+    let counter: ICounter;
+    let newCounter: ICounter;
+    let newCounters: ICounter[] = [];
 
-  switch (action.type) {
-    case TypeKeys.DECREMENTED:
-      return {
-        all: state.all.map((item) => {
-          if (item.index !== action.payload.index) {
-            // This isn't the item we care about - keep it as-is
-            return item;
+    switch (action.type) {
+      case TypeKeys.DECREMENTED:
+        return {
+          all: state.all.map((item) => {
+            if (item.index !== action.payload.index) {
+              // This isn't the item we care about - keep it as-is
+              return item;
+            }
+
+            // Otherwise, this is the one we want - return an updated value resetting all flags
+            return new Counter(action.payload.counter.index, action.payload.counter.value);
+          }),
+        };
+
+      case TypeKeys.INCREMENTED:
+        return {
+          all: state.all.map((item) => {
+            if (item.index !== action.payload.index) {
+              // This isn't the item we care about - keep it as-is
+              return item;
+            }
+
+            // Otherwise, this is the one we want - return an updated value resetting all flags
+            return new Counter(action.payload.counter.index, action.payload.counter.value);
+          }),
+        };
+
+      case TypeKeys.LOADED:
+        return {
+          all: state.all.map((item) => {
+            if (item.index !== action.payload.index) {
+              // This isn't the item we care about - keep it as-is
+              return item;
+            }
+
+            // Otherwise, this is the one we want - return an updated value
+            counter = new Counter(action.payload.counter.index, action.payload.counter.value);
+            return counter;
+          }),
+        };
+
+      case TypeKeys.LOADED_ALL:
+        const countersToAdd: ICounter[] = [];
+        for (const c of action.payload.counters) {
+          if (!state.all.find((item) => item.index === c.index)) {
+            countersToAdd.push(c);
           }
-
-          // Otherwise, this is the one we want - return an updated value resetting all flags
-          return new Counter(action.payload.counter.index, action.payload.counter.value);
-        }),
-      };
-
-    case TypeKeys.INCREMENTED:
-      return {
-        all: state.all.map((item) => {
-          if (item.index !== action.payload.index) {
-            // This isn't the item we care about - keep it as-is
-            return item;
-          }
-
-          // Otherwise, this is the one we want - return an updated value resetting all flags
-          return new Counter(action.payload.counter.index, action.payload.counter.value);
-        }),
-      };
-
-    case TypeKeys.LOADED:
-      return {
-        all: state.all.map((item) => {
-          if (item.index !== action.payload.index) {
-            // This isn't the item we care about - keep it as-is
-            return item;
-          }
-
-          // Otherwise, this is the one we want - return an updated value
-          counter = new Counter(action.payload.counter.index, action.payload.counter.value);
-          return counter;
-        }),
-      };
-
-    case TypeKeys.LOADED_ALL:
-      const countersToAdd: ICounter[] = [];
-      for (const c of action.payload.counters) {
-        if (!state.all.find((item) => item.index === c.index)) {
-          countersToAdd.push(c);
         }
-      }
 
-      // copy the state and add the recently loaded counters
-      newCounters = state.all.map((item) => item).concat(countersToAdd);
+        // copy the state and add the recently loaded counters
+        newCounters = state.all.map((item) => item).concat(countersToAdd);
 
-      // sort the state by counter index
-      newCounters = newCounters.sort((a: ICounter, b: ICounter) => {
-        return a.index - b.index;
-      });
+        // sort the state by counter index
+        newCounters = newCounters.sort((a: ICounter, b: ICounter) => {
+          return a.index - b.index;
+        });
 
-      // return the resulting state
-      return {
-        all: newCounters,
-      };
+        // return the resulting state
+        return {
+          all: newCounters,
+        };
 
-    case TypeKeys.LOADING:
-      /*
-       * This is the only case where the counter does probably not yet exist.
-       * It will be created and initialized with the counter and isLoading.
-       */
-      counter = state.all.find((item) => item.index === action.payload.index);
-      if (counter) {
-        // another LOADING action when the counter is already in the list means: do nothing.
+      case TypeKeys.LOADING:
+        /*
+         * This is the only case where the counter does probably not yet exist.
+         * It will be created and initialized with the counter and isLoading.
+         */
+        counter = state.all.find((item) => item.index === action.payload.index);
+        if (counter) {
+          // another LOADING action when the counter is already in the list means: do nothing.
+          return state;
+        }
+
+        // copy the state
+        newCounters = state.all.map((item) => item);
+
+        // add the counter with uninitialized value
+        newCounter = new Counter(action.payload.index);
+        newCounter.isLoading = true;
+        newCounters.push(newCounter);
+
+        // sort the state by counter index
+        newCounters = newCounters.sort((a: ICounter, b: ICounter) => {
+          return a.index - b.index;
+        });
+
+        // return the resulting state
+        return {
+          all: newCounters,
+        };
+
+      case TypeKeys.LOADING_ALL:
         return state;
-      }
 
-      // copy the state
-      newCounters = state.all.map((item) => item);
+      case TypeKeys.SAVING:
+        /*
+         * Get the counter we're saving so we can use its old value until the new one
+         * is retrieved from the server.
+         */
+        counter = state.all.find((item) => item.index === action.payload.index);
+        return {
+          all: state.all.map((item) => {
+            if (item.index !== action.payload.index) {
+              // This isn't the item we care about - keep it as-is
+              return item;
+            }
 
-      // add the counter with uninitialized value
-      newCounter = new Counter(action.payload.index);
-      newCounter.isLoading = true;
-      newCounters.push(newCounter);
+            newCounter = new Counter(counter.index, counter.value);
+            newCounter.isSaving = true;
+            return newCounter;
+          }),
+        };
 
-      // sort the state by counter index
-      newCounters = newCounters.sort((a: ICounter, b: ICounter) => {
-        return a.index - b.index;
-      });
-
-      // return the resulting state
-      return {
-        all: newCounters,
-      };
-
-    case TypeKeys.LOADING_ALL:
-      return state;
-
-    case TypeKeys.SAVING:
-      /*
-       * Get the counter we're saving so we can use its old value until the new one
-       * is retrieved from the server.
-       */
-      counter = state.all.find((item) => item.index === action.payload.index);
-      return {
-        all: state.all.map((item) => {
-          if (item.index !== action.payload.index) {
-            // This isn't the item we care about - keep it as-is
-            return item;
-          }
-
-          newCounter = new Counter(counter.index, counter.value);
-          newCounter.isSaving = true;
-          return newCounter;
-        }),
-      };
-
-    default:  // enforced by TypeKeys.OTHER_ACTION
-      return state;
-  }
-}
+      default:  // enforced by TypeKeys.OTHER_ACTION
+        return state;
+    }
+  };
--- src/app/reducers/errors.reducer.ts
+++ src/app/reducers/errors.reducer.ts
@@ -1,15 +1,17 @@
+import { Reducer } from "redux";
 import { ActionTypes, TypeKeys } from "../actions/counter.actions";
 import { INITIAL_ERRORS_STATE } from "../models/app-state";
 
-export function errorsReducer(state: string[] = INITIAL_ERRORS_STATE, action: ActionTypes): string[] {
-  switch (action.type) {
-    case TypeKeys.ERROR:
-      return [...state, action.error];
+export const errorsReducer: Reducer<string[]> =
+  (state: string[] = INITIAL_ERRORS_STATE, action: ActionTypes): string[] => {
+    switch (action.type) {
+      case TypeKeys.ERROR:
+        return [...state, action.error];
 
-    case TypeKeys.RESET_ERRORS:
-      return INITIAL_ERRORS_STATE;
+      case TypeKeys.RESET_ERRORS:
+        return INITIAL_ERRORS_STATE;
 
-    default:
-      return state;
-  }
-}
+      default:
+        return state;
+    }
+  };
