cd047294f4785c503892dbb4c3b3210e40c0b68e
Michael Kaaden <github@kaaden.net>
2018-04-16 21:32:35 +0200

    Add some tests

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,26 +1,108 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { MockNgRedux, NgReduxTestingModule } from "@angular-redux/store/lib/testing";
 
 import { DashboardComponent } from "./dashboard.component";
+import { CounterActionCreatorService } from "../../actions/counter.action-creator.service";
+import { Subject } from "rxjs/Subject";
+import { Counter, ICounter } from "../../models/counter";
+import { IAppState } from "../../models/app-state";
 
-xdescribe("DashboardComponent", () => {
+describe("DashboardComponent", () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let counterIndex: number;
+  let creatorSpy: jasmine.SpyObj<CounterActionCreatorService>;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [DashboardComponent],
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [DashboardComponent],
+      imports: [NgReduxTestingModule],
+      providers: [
+        {
+          provide: CounterActionCreatorService,
+          useValue: jasmine.createSpyObj("CounterActionCreatorService", ["loadAll"]),
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    MockNgRedux.reset();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    creatorSpy = TestBed.get(CounterActionCreatorService);
+    counterIndex = 0;
+
+    // prepare stubs
+    // creatorSpy.loadAll.and.returnValue({});
+
+    // fixture.detectChanges();  // will do this explicitly when needed
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should call loadAll on initialization", () => {
+    fixture.detectChanges();
+
+    expect(creatorSpy.loadAll.calls.count()).toBe(1, "loadAll was called once");
+  });
+
+  it("should select the counters from Redux", (done) => {
+    const countersStub: Subject<ICounter[]> = MockNgRedux.getSelectorStub<IAppState, ICounter[]>("counters");
+
+    // determine a sequence of values we'd like to test the Redux store with
+    const expectedValues: ICounter[][] = [
+      [new Counter(counterIndex, 42)],
+      [new Counter(counterIndex, 42), new Counter(counterIndex + 1, 43)],
+      [new Counter(counterIndex, 42), new Counter(counterIndex + 1, 43), new Counter(counterIndex + 2, 44)],
+    ];
+
+    // drive those values through our stub
+    expectedValues.forEach((counterArray: ICounter[]) => countersStub.next(counterArray));
+    // toArray only deals with completed streams
+    countersStub.complete();
+
+    // make sure counters$ receives these values
+    let i = 0;
+    component.counters$.subscribe(
+      (counterArray: ICounter[]) => {
+        // console.log(`received ${JSON.stringify(counterArray)}`);
+        expect(counterArray).toEqual(expectedValues[i++], "counters$ received the correct array");
+      },
+      (error) => console.log(error),
+      done
+    );
+  });
+
+  it("should select the counters length from Redux", (done) => {
+    const numOfCountersStub: Subject<number> = MockNgRedux.getSelectorStub<IAppState, number>(["counters", "length"]);
+
+    // determine a sequence of values we'd like to test the Redux store with
+    const expectedValues: ICounter[][] = [
+      [new Counter(counterIndex, 42)],
+      [new Counter(counterIndex, 42), new Counter(counterIndex + 1, 43)],
+      [new Counter(counterIndex, 42), new Counter(counterIndex + 1, 43), new Counter(counterIndex + 2, 44)],
+    ];
+
+    // drive those values through our stub
+    expectedValues.forEach((counterArray: ICounter[]) => numOfCountersStub.next(counterArray.length));
+    // toArray only deals with completed streams
+    numOfCountersStub.complete();
+
+    // make sure counters$ receives these values
+    let i = 0;
+    component.numOfCounters$.subscribe(
+      (length: number) => {
+        console.log(`got length ${length}`);
+        expect(length).toEqual(expectedValues[i++].length, "numOfCounters$ received the correct length");
+      },
+      (error) => console.log(error),
+      done
+    );
   });
 });
